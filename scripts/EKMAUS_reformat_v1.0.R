65e64232c7e0477adb34fa573e195ff3ff7ec5a3
estee <91368979+esteeschwarz@users.noreply.github.com>
2023-05-06 18:49:30 +0200

    upd

--- scripts/EKMAUS_reformat_v1.0.R
+++ scripts/EKMAUS_reformat_v1.0.R
@@ -3,11 +3,12 @@
 ################
 library(readtext)
 library(quanteda)
+library(stringi)
 getwd()
 #setwd("local/HU-LX/EKMAUS")
 files1<-list.files("expo")
 #setwd("expo")
-tra<-6
+tra<-5
 t1<-readLines(paste("expo",files1[tra],sep="/"))
 #t1
 m1<-grep("<M 1>",t1)
@@ -85,10 +86,14 @@ tl<-length(t1)
 # k<-581
 # k<-336
 # k<-484
-# tabm<-3
+ tabm<-2
 # length(m6)
 getlines<-function(t1,t2,m,tabm){
 set<-m
+# tabsep<-"\t"
+# tabm<-2
+# tabsepcom<-stri_join(rep(tabsep,tabm),collapse = "")
+
 for(k in 1:length(set)){
   tstart<-set[k]
   ifelse(k<length(set),
@@ -103,8 +108,9 @@ for(k in 1:length(set)){
     tlast<-grep("<END",t1[tstart1:tnext])+tstart-1
   tsp<-t1[set[k]]
   tabsep<-"\t"
-  tabsep<-stri_join(rep(tabsep,tabm),collapse = "")
-  t2$edit1[tstart]<-paste0("*",tsp,":",tabsep,paste(t1[tstart1:tlast],collapse  = " "))
+#  tabm<-2
+  tabsepcom<-stri_join(rep(tabsep,tabm),collapse = "")
+  t2$edit1[tstart]<-paste0("*",tsp,":",tabsepcom,paste(t1[tstart1:tlast],collapse  = " "))
   
 }
 #m<-t2$edit1!=0
@@ -113,10 +119,10 @@ for(k in 1:length(set)){
 return(t2)
 }
 t2<-data.frame(id=1:length(t1),edit1=0,edit2=0)
-t3<-getlines(t1,t2,m6,4)
+t3<-getlines(t1,t2,m6,2)
 t4<-t3
 if(!int2true)
-   t4<-getlines(t1,t3,m7,4)
+   t4<-getlines(t1,t3,m7,2)
 
 t5<-getlines(t1,t4,m8,1)
 m<-t5$edit1!=0
@@ -124,6 +130,9 @@ t6<-t5$edit1[m]
 #t6
 tns<-files1[tra]
 writeLines(t6,paste("reformatted",tns,sep = "/"))
+dir.create("EKMAUS_CHAT_reformatted")
+writeLines(t6,paste("EKMAUS_CHAT_reformatted",tns,sep = "/"))
+
 getwd()
 # for(k in 1:length(m8)){
 #   tstart<-m8[k]
