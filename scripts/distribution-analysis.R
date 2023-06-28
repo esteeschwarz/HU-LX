12311d0487a7a79288647d93c8a48e46c6ee1812
estee <91368979+esteeschwarz@users.noreply.github.com>
2023-06-28 19:20:11 +0200

    upd

--- scripts/distribution-analysis.R
+++ scripts/distribution-analysis.R
@@ -12,8 +12,8 @@ d1$L1[m]<-"G"
 
 d1$feature<-gsub("c_","",d1$feature)
 par(las=3)
-boxplot(d1$count~d1$kid)
-boxplot(d1$count~d1$feature)
+boxplot(d1$count~d1$kid,main="coded feature sums over targetchilds")
+boxplot(d1$count~d1$feature, main="coded features over corpus")
 
 s<-lmer(count~feature+(1|L1),d1)
 s<-lmer(count~L1+(1|feature),d1)
