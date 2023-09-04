5a7f84e1a0147eb95a44ba124ac678b5542991e5
estee <91368979+esteeschwarz@users.noreply.github.com>
2023-09-04 13:16:59 +0200

    get mdb tabe of metadata export

--- scripts/edit-status-meta.R
+++ scripts/edit-status-meta.R
@@ -10,19 +10,104 @@ root<-"~/Documents/Github/HU-LX"
 root<-"~/boxHKW/21S/DH/local/HU-LX"
 #src<-paste(root,"data/metadata_export_collection14_20230629-18_50.csv",sep = "/")
 src<-paste(root,"meta/metadata_export_collection14_20230821-11_46.csv",sep = "/")
+src<-paste(root,"meta/metadata_export_collection14_20230821-17_45.csv",sep = "/")
+src<-paste(root,"meta/metadata_export_collection14_20230904-09_22.csv",sep = "/")
+src<-paste(root,"meta/metadata_export_collection14_20230904-11_30.csv",sep = "/")
 
 d1<-read.csv(src)
 k.u<-unique(d1$Target.child[d1$Ressourcen.Typ=="Transcript"]) #42
 g.pdf<-grepl("pdf",d1$Original.filename)
 d11<-d1[!g.pdf,] # only resources not pdf
-k.u<-unique(d11$Target.child[d11$Ressourcen.Typ=="Transcript"]) #32
-k.u<-unique(d11$Target.child[d11$Ressourcen.Typ=="Audio"]) #32
+k.u.sp<-unique(d11$Target.child[d11$Ressourcen.Typ=="Transcript"]) #32
+k.u.au<-unique(d11$Target.child[d11$Ressourcen.Typ=="Audio"]) #32
+
+# spk_array<-c("GCA","GCB","GCC","GCD","GCE","GCF","GCG","GDA","GDB","GDC","GDD","GDE","GDF","TAA",
+#              "TAB","TAD","TAF","TAG","TAH","TAI","TBB","TBC","TBD","TBE","TBF","TBG","TBH","TBI",
+#              "TBK","TBL","TBM","TBN","TBO","TBP","TBQ","TBR","TBS","TBT","TBU","TBV")
+sketchdir<-"~/boxHKW/21S/DH/local/HU-LX/000_SES_REFORMATTED_transcripts/Formatted with header info/text/docx-txt/sketchmode/v3.4.1/version without header for SketchEngine upload"
+headerdir<-"~/boxHKW/21S/DH/local/HU-LX/000_SES_REFORMATTED_transcripts/Formatted with header info/text/docx-txt/SES_transcripts_w_header_v3_4_13302"
+#all available transformed and uploaded transcripts:
+
+chkspk<-list.files(sketchdir)
+spkns<-stri_split_regex(chkspk,"_",simplify = T)
+length(spkns[,2])
+spk_array<-c(spkns[,2])
+m<-spk_array %in% k.u.sp
+spk_array[which(!m)] #which transcript not in berlangdev / GCD: but filename in db false...
+chkspk<-list.files(headerdir)
+
+m<-spk_array %in% k.u.au
+spk_array[which(!m)] #which audio not in berlangdev 
+write_clip(spk_array[which(!m)])
+
 
 write_clip(k.u)
 
+### create markdown table of DF
+m<-c(1,8,7,length(d1))
+df.md<-d11[,m]
+df.md<-df.md[order(df.md$Target.child),]
+#sample md:
+"
+|column|explanation|example|
+|:--|:---|:---|
+|p_interview|transcript|GCA|
+|p_speaker|speaker|#GCA|
+"
+df.md.h<-paste0("|",paste(colnames(df.md),collapse = "|"),"|")
+df.md.1<-paste0("|",paste(rep(":--",length(df.md)),collapse = "|"),"|")
+df.md.x<-array()
+k<-1
+for(k in 1:length(df.md[,1])){
+  print(k)
+  df.md.x[k]<-paste0("|",paste(df.md[k,],collapse = "|"),"|")
+  
+}
+library(xfun)
+df.status<-data.frame(child=spk_array,CHAT=0,sanscodes=0,pdf=0,audio=0,missing=0)
+k<-24
+for(k in 1:length(d1$Ressourcen.ID.s.)){
+  file.ns<-file_ext(d1$Original.filename[k])
+  file.sep<-stri_split_regex(d1$Title[k],"_",simplify = T)
+  file.pdf<-grep("pdf",d1$Original.filename[k])
+  kid<-d1$Target.child[k]
+  pos<-which(df.status$child==kid)
+  ifelse ("CHAT" %in% file.sep,
+    df.status$CHAT[pos]<-1,df.status$missing[pos]<-paste(df.status$missing[pos],"CHAT",collapse = "/"))
+  ifelse ("sanscodes" %in% file.sep,
+    df.status$sanscodes[pos]<-1,df.status$missing[pos]<-paste(df.status$missing[pos],"sanscodes",collapse = "/"))
+  ifelse ("anon" %in% file.sep,
+    df.status$audio[pos]<-1,df.status$missing[pos]<-paste(df.status$missing[pos],"audio",collapse = "/"))
+  ifelse (length(file.pdf)>=1,
+    df.status$pdf[pos]<-1,df.status$missing[pos]<-paste(df.status$missing[pos],"pdf",collapse = "/"))
+}
+df.status.c<-df.status[,1:5]
+getmd<-function(set){
+  
+df.md<-set
+df.md.h<-paste0("|",paste(colnames(df.md),collapse = "|"),"|")
+df.md.1<-paste0("|",paste(rep(":--",length(df.md)),collapse = "|"),"|")
+df.md.x<-array()
+k<-1
+for(k in 1:length(df.md[,1])){
+  print(k)
+  df.md.x[k]<-paste0("|",paste(df.md[k,],collapse = "|"),"|")
+  
+}
 
 
+df.md.x
+#df.md.c<-c(df.md.h,df.md.x)
+df.md.head<-"## SES: BERLANGDEV media status
+"
+return(df.md.c<-c(df.md.head,df.md.h,df.md.1,df.md.x))
+}
+df.md.c<-getmd(df.status.c)
+#df.md.c.o<-df.md.c[order(df.md.c[,2])]
 
+write_clip(df.md.c)
+getwd()
+writeLines(df.md.c,"ses-status.md")
 #src<-paste(root,"meta/metadata_export_collection14_20230705-06_27.csv",sep = "/")
 #d1<-read.csv(src)
 
