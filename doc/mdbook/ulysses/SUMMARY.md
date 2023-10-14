3aea2eb785213e6f32f983d7b3c598ce7a6552c3
estee <91368979+esteeschwarz@users.noreply.github.com>
2023-10-14 11:10:13 +0200

    upd wrapup

---
+++ doc/mdbook/ulysses/SUMMARY.md
@@ -0,0 +1,41 @@
+# Summary
+
+- [content][1]
+- [SES Database][7]
+	- [columns explained][8]
+	- [possibilities][9]
+- [anonymize interview audios][19]
+- [Sketch Engine][2]
+	- [log in to Sketch Engine][3]
+	- [create new corpus][4]
+	- [import texts][5]
+	- [compile corpus][6]
+- [ANNIS framework][16]
+- [BERLANGDEV workflow][17]
+- [BERLANGDEV media status][18]
+- [SES findings][10]
+	- [001][11]
+	- [002][12]
+	- [003][13]
+	- [accountability][14]
+	- [distribution analysis][15]
+
+[1]:	a_intro.md
+[2]:	g_pageske0.md
+[3]:	h_page001.md
+[4]:	i_page003.md
+[5]:	j_page004.md
+[6]:	k_page005.md
+[7]:	c_sesdb01.md
+[8]:	d_sesdb002.md
+[9]:	e_sesdb003.md
+[10]:	o_findings00.md
+[11]:	p_findings01.md
+[12]:	q_findings02.md
+[13]:	r_findings03.md
+[14]:	s_sesdb004.md
+[15]:	t_sesdist001.md
+[16]:	l_annis01.md
+[17]:	m_berlangdev01.md
+[18]:	n_ses-status.md
+[19]:	f_audacity.md
--- scripts/wrapup-print.R
+++ scripts/wrapup-print.R
@@ -28,34 +28,40 @@ f.df$head[k]<-readLines(paste(srcdir,f[f.md.w[k]],sep = "/"))[1]
 f.df$page[k]<-  readtext(paste(srcdir,f[f.md.w[k]],sep = "/"))$text
 
 }
-f.df.o<-d[order(d$pos.m),]
+dir.git<-"~/Documents/GitHub/HU-LX"
+d<-read_csv("~/boxhkw/21s/dh/local/hu-lx/doc/ses-wrapup.csv")
 
-f.df.o$page<-gsub("^#{2,4}","#",f.df.o$page) #turn all top header to h1 header
+f.df.o<-d[order(d$pos.m),]
+f.df.o<-f.df[order(f.df$pos),]
+
+#f.df.o$page<-gsub("^#{2,4}","#",f.df.o$page) #turn all top header to h1 header
 ###
 #combine pages
-writeLines(f.df.o$page[2:length(f.df.o$page)],"mdbook/ses-wrapup-raw.md")
-f.df.o$file.index<-letters[1:length(f.df.o$file)]
-f.df.o$file.sort<-paste0(f.df.o$file.index,"_",f.df.o$file)
-dir.create("ulysses")
+writeLines(f.df.o$page[2:length(f.df.o$page)],paste(dir.git,"doc/mdbook","ses-wrapup-raw.md",sep = "/"))
+#f.df.o$file.index<-letters[1:length(f.df.o$file)]
+#f.df.o$file.sort<-paste0(f.df.o$file.index,"_",f.df.o$file)
+dir.create(paste(dir.git,"doc/mdbook/ulysses",sep = "/"))
+k<-1
 for(k in 1:length(f.df.o$file)){
-  writeLines(f.df.o$page[k],paste("ses-md",f.df.o$file.sort[k],sep = "/"))
+  writeLines(f.df.o$page[k],paste(dir.git,"doc/mdbook/ulysses",f.df.o$file[k],sep = "/"))
 }
-write_csv(f.df.o,"/volumes/ext/boxhkw/21s/dh/local/hu-lx/doc/ses-wrapup.csv")
+#write_csv(f.df.o,"/volumes/ext/boxhkw/21s/dh/local/hu-lx/doc/ses-wrapup.csv")
 
-d<-read_csv("/volumes/ext/boxhkw/21s/dh/local/hu-lx/doc/ses-wrapup.csv")
+#d<-read_csv("/volumes/ext/boxhkw/21s/dh/local/hu-lx/doc/ses-wrapup.csv")
 m<-which(d$pos.m!=0)
 k<-20
-for(k in m[1]:length(d$file)){
-  writeLines(d$page[k],paste("ses-mdb",d$file[k],sep = "/"))
-}
+dir.create(paste(dir.git,"doc/mdbook/ses-mdb",sep = "/"))
+# for(k in m[1]:length(d$file)){
+#   writeLines(d$page[k],paste(dir.git,"doc/mdbook/ses-mdb",d$file.sort[k],sep = "/"))
+# }
 for(k in 1:length(d$file)){
-  writeLines(d$page[k],paste("ses-mdb",d$file[k],sep = "/"))
-}
+ writeLines(d$page[k],paste(dir.git,"doc/mdbook/ses-mdb",d$file.sort[k],sep = "/"))
+ }
 regx<-"((?<=:\t)(.*)(?=.png))"
 repl<-"https://ada-sub.dh-index.org/school/pr/2023-04-15/ses_wrapup/src/\\1"
 d$page[1:19]<-gsub(regx,repl,d$page[1:19],perl = T)
 d$page[1]
-md.sum<-readtext("ses-mdb/SUMMARY.md")$text
+md.sum<-readtext(paste0(dir.git,"/doc/mdbook/ses-mdb/a_SUMMARY.md"))$text
 md.sum
 f.df.o$page[1]<-md.sum
 #d$page[20]<-f.df.o$page[20]
@@ -79,3 +85,5 @@ which(f.df$file==f.sum.o.cl[1])
 k<-1
 which(f.df$file[k]==f.sum.o.cl)
 
+library(rmarkdown)
+render(paste(dir.git,"doc/mdbook/ses-wrapup-raw.Rmd",sep = "/"))
