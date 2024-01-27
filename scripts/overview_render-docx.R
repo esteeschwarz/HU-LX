27b3420ad4aaf2bd988fc2f1b0933f5e9748e4e0
st <91368979+esteeschwarz@users.noreply.github.com>
2024-01-27 19:52:54 +0100

    Update overview_render-docx.R, abs pages pth

--- scripts/overview_render-docx.R
+++ scripts/overview_render-docx.R
@@ -3,12 +3,12 @@
 ####################################
 .libPaths(new = "/home/runner/work/HU-LX/HU-LX/rlibs", include.site = TRUE)
 library(rmarkdown)
-render("../pages/overview.md.child.Rmd")
-tmd<-readLines("../pages/overview.md.child.md")
+render("pages/overview.md.child.Rmd")
+tmd<-readLines("pages/overview.md.child.md")
 m<-grep("\\\\\\[[a-zA-Z.]{1,12}\\\\\\]",tmd)
 tmd[m]
 #gsub("\\\\\\[([a-zA-Z.]{1,9})\\\\\\]","[\\1]",tmd[m])
 tmd[m]<-gsub("\\\\\\[([a-zA-Z.]{1,12})\\\\\\]","[\\1]",tmd[m])
-writeLines(tmd,"../pages/pfaff-corpus-class.md")
-render("../pages/overview.docx.parent.Rmd")
+writeLines(tmd,"pages/pfaff-corpus-class.md")
+render("pages/overview.docx.parent.Rmd")
 #file.copy("..pages/overview.docx.parent.docx","../pages/pfaff-corpusclass-overview.docx",overwrite = T)
