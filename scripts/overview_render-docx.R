ea971b6b928bb6458f057482a925626bd512f2a8
esteeschwarz <91368979+esteeschwarz@users.noreply.github.com>
2024-01-28 07:30:42 +0100

    upload artifact

--- scripts/overview_render-docx.R
+++ scripts/overview_render-docx.R
@@ -3,12 +3,13 @@
 ####################################
 .libPaths(new = "/home/runner/work/HU-LX/HU-LX/rlibs", include.site = TRUE)
 library(rmarkdown)
-render("pages/overview.md.child.Rmd")
-tmd<-readLines("pages/overview.md.child.md")
+getwd()
+render("./pages/overview.md.child_FIRST.Rmd")
+tmd<-readLines("./pages/overview.md.child_FIRST.md")
 m<-grep("\\\\\\[[a-zA-Z.]{1,12}\\\\\\]",tmd)
 tmd[m]
 #gsub("\\\\\\[([a-zA-Z.]{1,9})\\\\\\]","[\\1]",tmd[m])
-tmd[m]<-gsub("\\\\\\[([a-zA-Z.]{1,12})\\\\\\]","[\\1]",tmd[m])
-writeLines(tmd,"pages/pfaff-corpus-class.md")
-render("pages/overview.docx.parent.Rmd")
+tmd[m]<-gsub("\\\\\\[([a-zA-Z.]{1,12})\\\\\\]","[\\1]",tmd[m]) # replace relative image links ![][image-1] with abs
+writeLines(tmd,"./pages/pfaff_corpus-class-overwiew.md")
+render("./pages/pfaff_corpus-class-overview.Rmd")
 #file.copy("..pages/overview.docx.parent.docx","../pages/pfaff-corpusclass-overview.docx",overwrite = T)
