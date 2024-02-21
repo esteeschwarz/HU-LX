b10bd78dc9fbf69514cd0317eb0b6967111cf3e3
esteeschwarz <91368979+esteeschwarz@users.noreply.github.com>
2024-02-21 19:42:07 +0100

    rename LLDM

--- scripts/LLDM_filerenames.R
+++ scripts/LLDM_filerenames.R
@@ -50,4 +50,79 @@ f.rn<-f[m]
 f.rn
 load("~/boxHU/work/particip.df.cpt_all-p-onecolumn.RData")
 ####
+#14085.rename LLDM
+library(stringi)
+library(tools)
 
+kids<-unique(p.df.c$participant)
+projects<-c("SESB","OSZ","RKO")
+years<-c("10","12")
+types<-c("ON","OE","WN","WE","on","oe","we","wn")
+langs<-c("DE","EN","de","en")
+filetemp<-data.frame(file=p.df.c$file,renamed=NA,project=NA,year=NA,participant=NA,lang=NA,type=NA,info=NA,ext=NA)
+k<-2
+for(k in 1:length(filetemp$file)){
+  file=filetemp$file[k]
+  ext<-file_ext(file)
+#p<-2
+  for (p in 1:length(kids)){
+  kid<-kids[p]
+  m1<-stri_extract_all_regex(file,kid,simplify = T)
+  if (!is.na(m1))
+    filetemp$participant[k]<-m1  
+  }
+  for (y in 1:length(years)){
+    year<-years[y]
+    yregx<-paste0("([OSZRKOSESB-_]",year,"[-_])")
+    
+    m1<-stri_extract_all_regex(file,yregx,simplify = T)
+    if (!is.na(m1))
+      filetemp$year[k]<-m1  
+  }
+  for (l in 1:length(langs)){
+    lang<-langs[l]
+    lregx<-paste0("([-_]",lang,"[-_.])")
+    m1<-stri_extract_all_regex(file,lregx,simplify = T)
+    if (!is.na(m1))
+      filetemp$lang[k]<-m1  
+  }
+  for (t in 1:length(types)){
+    type<-types[t]
+    tregx<-paste0("([-_]",type,"[-_.])")
+    m1<-stri_extract_all_regex(file,tregx,simplify = T)
+    if (!is.na(m1))
+      filetemp$type[k]<-m1  
+  }
+  for (pr in 1:length(projects)){
+    project<-projects[pr]
+    prregx<-paste0("([-_]",project,"[-_])")
+    prregx<-project
+    m1<-stri_extract_all_regex(file,prregx,simplify = T)
+    if (!is.na(m1))
+      filetemp$project[k]<-m1  
+  }
+  mna<-!is.na(filetemp[k,3:7])
+  relev<-filetemp[k,3:7][mna]
+  alii<-paste0(relev,collapse = "|")
+  aregx<-paste0("(",alii,"|\\.",ext,")")
+  rmall<-gsub(aregx,"",filetemp$file[k])
+  filetemp$info[k]<-rmall
+  filetemp$ext[k]<-ext
+  ### TODO: OLIVER-OSC > not VER
+  ### info DEOE etc. rm
+  
+  
+}
+filetemp$year<-gsub("[^0-2]","",filetemp$year)
+filetemp$lang<-toupper(filetemp$lang)
+filetemp$lang<-gsub("[^DEN]","",filetemp$lang)
+filetemp$type<-toupper(filetemp$type)
+filetemp$type<-gsub("[^WEOEWNON]","",filetemp$type)
+filetemp$type<-toupper(filetemp$type)
+filetemp$info<-gsub("(OE|WE|WN|ON|")
+filetemp$renamed<-paste0(filetemp$project,"-",filetemp$year,"_",
+                         filetemp$participant,"_",filetemp$lang,"_",filetemp$type,"_",
+                         filetemp$info,".",filetemp$ext)
+is.na(m1)
+length(m1)
+kids
