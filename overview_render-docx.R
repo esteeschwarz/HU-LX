2356489b552e78ce26581a490eca9e9bf215ed4e
estee-lapsi <91368979+esteeschwarz@users.noreply.github.com>
2026-01-20 16:24:28 +0100

    wf,7

--- overview_render-docx.R
+++ overview_render-docx.R
@@ -1,7 +1,9 @@
 #20240121(07.14)
 #14042.overview-docx.from.mdbook-src
 ####################################
+### critical
 .libPaths(new = "/home/runner/work/HU-LX/HU-LX/rlibs", include.site = TRUE)
+###
 library(rmarkdown)
 library(bookdown)
 
