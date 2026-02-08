757ef06702c9c6c55706a46637125be6cdbeeb31
estee-lapsi <91368979+esteeschwarz@users.noreply.github.com>
2026-02-08 11:47:02 +0100

    upd renaming script

--- scripts/EKMAUS-rename.R
+++ scripts/EKMAUS-rename.R
@@ -1,7 +1,10 @@
 folder<-"/Users/guhl/Documents/boxMINI/SNC/M8/lade/Kol_ID10-original_wk"
+folder<-"/Users/guhl/Documents/boxMINI/SNC/M8/lade/Kol_ID14-original"
+folder<-"/Volumes/NAS4-MAC/cloud/boxHU/Kol_ID10-original"
 dirs<-list.dirs(folder,full.names = T)
 dirs
-d<-dirs[2]
+#d<-dirs[2]
+d<-dirs
 setwd(d)
 dl<-"."
 f<-list.files(dl,full.names = T)
@@ -16,16 +19,56 @@ s
 reen.dep<-function(){
 ### this wks.
 ### has to read in files again to apply gsubs
+# quick rename wo changing
+# chk SES first
+s
+### SES
 s1<-lapply(s,function(x){
-  n<-paste0(x[2:length(x)],collapse = "_")
+  m<-ifelse(length(x)>1,x[2]=="SES",F) # chk
+#  m<-m[!is.na(m)]
+  n<-paste0(x,collapse = "_")
+  if(m)
+    n<-paste0(x[2:length(x)],collapse = "_")
+  return(n)
 })
+### EKMAUS
+s1<-lapply(s,function(x){
+  m<-ifelse(length(x)>1,grepl("/RS",x[1]),F) # chk
+  #  m<-m[!is.na(m)]
+  n<-paste0(x,collapse = "_")
+  if(m)
+    n<-paste0(x[2:length(x)],collapse = "_")
+  return(n)
+})
+s
+#warnings()
 s1
 f
 dl
-sum(duplicated(s1))
-file.rename(f,unlist(s1))
-f<-list.files(dl)
 f
+library(tools)
+
+dp<-sum(duplicated(s1))
+dp
+if(dp>0){
+  m<-which(duplicated(s1))
+  s1[m]
+  bf<-file_path_sans_ext(s1[m])
+  bf
+  bf<-paste(bf,"2",sep  = "-")
+  bf<-paste(bf,file_ext(s1[m]),sep = ".")
+  bf
+  s1[m]<-bf
+}
+dp<-sum(duplicated(s1))
+dp
+if(dp==0){
+  print("no duplicates, renaming...")
+  file.rename(f,unlist(s1))
+}
+f2<-list.files(dl)
+f2
+#############################
 f2<-gsub("_geschw..rzt","",f)
 sum(duplicated(f2))
 f2<-gsub(" ","",f2)
@@ -41,13 +84,18 @@ f2
 file.rename(f,f2)
 }
 s
+### quick rename
+
+
+
+
 library(abind)
 sa<-abind(s)
 sdf<-data.frame(matrix(abind(s,along=1),ncol = length(s[[1]]),byrow = T))
-write.csv(sdf,"~/Documents/boxMINI/SNC/M8/lade/Kol_ID10-original_wk/temp-rename-audio-tr.csv")
-md<-lapply(s, function(x){
-  
-})
+#write.csv(sdf,"~/Documents/boxMINI/SNC/M8/lade/Kol_ID10-original_wk/temp-rename-audio-tr.csv")
+# md<-lapply(s, function(x){
+#   
+# })
 s2<-gsub("geschw..rzt","",s2)
 s2<-gsub("geschw..rzt","",s2)
 sum(duplicated(s2))
